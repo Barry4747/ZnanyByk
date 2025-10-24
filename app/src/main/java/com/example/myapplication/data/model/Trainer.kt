e8e8030322a1fb3e21cf97de2392346c85d3b837
Marcel <marcel.mal3cki@gmail.com>
2025-10-24 13:45:18 +0200

    Dodanie trenerów w homepagu

--- app/src/main/java/com/example/myapplication/data/model/Trainer.kt
+++ app/src/main/java/com/example/myapplication/data/model/Trainer.kt
@@ -1,11 +1,16 @@
 package com.example.myapplication.data.model
 
-data class Trainer (
-    val trainer_id: String,
-    val description: String="",
+data class Trainer(
+    val id: String = "",
+    val name: String = "", 
+    val description: String = "",
     val specialities: List<String> = emptyList(),
-    val location: String,
+    val location: String = "",
     val ratings: List<Int> = emptyList(),
-    val priceHour: Int,
-    val experience: Float
-)
+    val pricePerHour: Int = 0,
+    val experience: Float = 0f,
+    val email: String = ""
+) {
+
+    constructor() : this("", "", "", emptyList(), "", emptyList(), 0, 0f, "")
+}
--- app/src/main/java/com/example/myapplication/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/home/HomeScreen.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication.ui.screens.home
 
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -7,21 +9,32 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.myapplication.data.model.Trainer
 import com.example.myapplication.viewmodel.HomeViewModel
+
 
 @Composable
 fun HomeScreen(
@@ -31,51 +44,52 @@ fun HomeScreen(
 ) {
     val homeState by viewModel.homeState.collectAsState()
     val user = homeState.user
+    val trainers = homeState.trainers
     val errorMessage = homeState.errorMessage
+
+    var searchTrainerText by remember { mutableStateOf("") }
 
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .padding(12.dp),
+        verticalArrangement = Arrangement.Top
     ) {
         Text(
-            text = "Witaj!",
+            text = "Znajdź swojego trenera",
             style = MaterialTheme.typography.headlineLarge,
             fontWeight = FontWeight.Bold
         )
 
         Spacer(modifier = Modifier.height(32.dp))
 
+        OutlinedTextField(
+            value = searchTrainerText,
+            onValueChange = { searchTrainerText = it},
+            label = { Text("Wyszukaj") },
+            modifier = Modifier.fillMaxWidth(),
+            leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search Icon"
+            )
+        }, singleLine = true
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyColumn(
+            modifier = Modifier.weight(1f), // weight(1f) sprawia, że lista zajmie całą dostępną przestrzeń
+            verticalArrangement = Arrangement.spacedBy(12.dp) // Odstęp między elementami
+        ) {
+            items(trainers) { trainer ->
+                TrainerProfileCard(trainer = trainer, onClick = { /* TODO: Obsłuż kliknięcie */ })
+            }
+        }
+
         if (homeState.isLoading) {
             CircularProgressIndicator()
-        } else if (user != null) {
-            Card(
-                modifier = Modifier.fillMaxWidth(),
-                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
-            ) {
-                Column(
-                    modifier = Modifier.padding(16.dp)
-                ) {
-                    Text(
-                        text = "Profil Użytkownika",
-                        style = MaterialTheme.typography.titleLarge,
-                        fontWeight = FontWeight.Bold
-                    )
-
-                    Spacer(modifier = Modifier.height(16.dp))
-
-                    UserDataRow(label = "First Name", value = user.firstName)
-                    Spacer(modifier = Modifier.height(8.dp))
-
-                    UserDataRow(label = "Last Name", value = user.lastName)
-                    Spacer(modifier = Modifier.height(8.dp))
-
-                    UserDataRow(label = "Email", value = user.email)
-                }
-            }
-        } else if (errorMessage != null) {
+        }  else if (errorMessage != null) {
             Card(
                 modifier = Modifier.fillMaxWidth(),
                 elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
@@ -115,6 +129,37 @@ fun HomeScreen(
             modifier = Modifier.fillMaxWidth()
         ) {
             Text("Wyloguj się")
+        }
+    }
+
+}
+
+@Composable
+fun TrainerProfileCard(
+    trainer: Trainer,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = trainer.name,
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = trainer.description,
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.secondary
+            )
         }
     }
 }
