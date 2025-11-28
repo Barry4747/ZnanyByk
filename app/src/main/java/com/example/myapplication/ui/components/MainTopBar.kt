f5a2907718f6932e5e34f700d326d93e2480f4de
Barry4747 <bartek.rasztar24@gmail.com>
2025-11-28 13:21:08 +0100

    MainTopBar added

---
+++ app/src/main/java/com/example/myapplication/ui/components/MainTopBar.kt
@@ -0,0 +1,53 @@
+package com.example.myapplication.ui.components
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.components.buttons.MainBackButton
+
+@Composable
+fun MainTopBar(onNavigateBack: () -> Unit, text: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(72.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            MainBackButton(onClick = onNavigateBack)
+
+            Text(text = text, style = MaterialTheme.typography.titleLarge)
+        }
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .height(8.dp)
+                .background(
+                    brush = Brush.verticalGradient(
+                        listOf(Color.Black.copy(alpha = 0.15f), Color.Transparent)
+                    )
+                )
+        )
+    }
+}
